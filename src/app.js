3ea819ef24e3d4c3e39795875a6551dd1bdd0136
tanzimshahriar <tanzimshahriarfahim@gmail.com>
2019-09-13 12:13:20 +1000

    Users when registered email and password is stored in users table

--- src/app.js
+++ src/app.js
@@ -1,50 +1,28 @@
-// Import the Google Cloud client library
-'use strict';
-
-// [START gae_node_request_example]
 const express = require('express');
-
 const app = express();
+var bodyParser = require ('body-parser');
+
+// set up global middleware
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+
+//set and use routers
+const userRouter = require('./routes/userRouter.js');
+app.use(userRouter);
 
 app.get('/', (req, res) => {
   res
     .status(200)
-    .send('tom@gmail added to datastore')
+    .send('This is the api')
     .end();
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-
-const {BigQuery} = require('@google-cloud/bigquery');
-
-async function insertRowsAsStream() {
-  // Inserts the JSON objects into my_dataset:my_table.
-
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  // const datasetId = 'my_dataset';
-  // const tableId = 'my_table';
-  // const rows = [{name: 'Tom', age: 30}, {name: 'Jane', age: 32}];
-  const datasetId = 'bigquery_a2_dataset';
-  const tableId = 'users';
-  const row = [{email: 'tom@gmail.com', password: 3000}];
-
-  // Create a client
-  const bigqueryClient = new BigQuery();
-
-  // Insert data into a table
-  await bigqueryClient
-    .dataset(datasetId)
-    .table(tableId)
-    .insert(row);
-  console.log(`Inserted ${row.length} rows`);
-}
-insertRowsAsStream();
 
 module.exports = app;
