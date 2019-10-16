06366713ead6c3561a492f345b29c5626fc50a67
tanzimshahriar <tanzimshahriarfahim@gmail.com>
2019-10-16 19:39:42 +1100

    purchaseitems somewhatworking

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -7,49 +7,80 @@ productsRouter.get('/getproducts', (req, res) => {
     connection.query("SELECT name, description, imagesrc, price, quantity FROM products",
         async function (error, results, fields) {
             try {
-                
+
                 var products = []
-                
+
                 for (var i = 0; i < results.length; i++) {
-                    
+
                     products.push(results[i])
                 }
 
                 res.status(200).json({
                     "msg": "Success",
                     "products": products
-                }) 
-            }
-            catch {
+                })
+            } catch {
                 res.status(400).json({
                     "msg": "Failed to fetch products",
                     "ErrorCode": "SQL_ERROR",
                 })
             }
         })
-                
+
 })
 
-productsRouter.post('/purchaseItems', async (req, res) => {
-    try {
-    //   const user = {
-    //     email: req.body.email,
-    //     password: hashedPassword,
-    //     secretToken: secretToken,
-    //   }
-    //   connection.query('INSERT INTO users SET ?', user,
-    //     async function (error, results, fields) {
-    //         res.status(200).json({
-    //           result: "Success"
-    //         })
-    //     })
-        
-        res.status(200).json({
-            result: "Success"
-        })
-    } catch {
-      res.status(500).json({msg:"failed to purchase"});
+productsRouter.post('/purchaseitems', (req, res) => {
+    //connection.query('INSERT INTO users SET ?', user,
+    var datetime = require('node-datetime');
+    var errors = [];
+    var dt = datetime.create();
+    var formatted = dt.format('Y-m-d H:M:S');
+    const purchaseid= -1;
+    const quanitity = -1;
+    console.log(req.body.items)
+    const purchase = {
+        email: req.body.email,
+        items: req.body.items,
+        datetime: formatted
     }
-  });
+    console.log(purchase)
+    connection.query("INSERT into purchases SET ?", purchase,
+        function (error, results, fields) {
+            if(!error){
+                errors.push(error)
+            }
+        })
+    // connection.query("SELECT @@id from purchases", function (error, results, fields) {
+    //     if(!(error || errors)) {
+    //         errors.push(error)
+    //     } else {
+    //         purchaseid = results.id;
+    //         quanitity = results.quanitity-1;
+    //         console.log(purchaseid)
+    //     }
+    // })
+    // connection.query("Update products Set quantity=? Where id=?", [quanitity,purchaseid],function (error, results, fields) {
+    //     if(!(error || errors)) {
+    //         errors.push(error)
+    //     } else {
+    //         purchaseid = results[0].id
+    //         console.log(purchaseid)
+    //     }
+    // })
+    if(errors.length>0){
+        res.status(400).json({
+            result: "Failed",
+            errors: errors
+        })
+    }
+    else {
+        res.status(200).json({
+            result: "Success",
+            msg: "Purchase Successful",
+        })
+    }
+    
+
+})
 
 module.exports = productsRouter
